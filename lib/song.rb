56b953fdbda897041255e55a36e238fa03f2de93
Jane Wysoki <jane.wysoki@gmail.com>
2019-08-19 16:22:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,10 +41,10 @@ class Song
     @@all.sort_by{|x| x.name}
   end
   
-  def self.new_from_filename(filename)
+  def self.new_from_filename(name)
     song = self.new 
-    song.filename = (filename.split(" - ")[1].chomp(".mp3"))
-    song.artist_name = (filename.split(" - ")[0])
+    song.name = (name.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (name.split(" - ")[0])
     song
   end
   
