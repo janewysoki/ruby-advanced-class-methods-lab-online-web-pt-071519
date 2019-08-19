361c1b9fcbd4a74df77946ece40da3de006caf20
Jane Wysoki <jane.wysoki@gmail.com>
2019-08-19 16:22:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -42,7 +42,10 @@ class Song
   end
   
   def self.new_from_filename(filename)
-    
+    song = self.new 
+    song.filename = (filename.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (filename.split(" - ")[0])
+    song
   end
   
   def self.create_from_filename(filename)
